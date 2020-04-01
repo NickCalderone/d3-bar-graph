c342c76198cc1362d8c80f01de568e5791feb053
nick <calderonenick@gmail.com>
2020-04-01 16:40:46 -0600

    passed all checks

--- script.js
+++ script.js
@@ -12,7 +12,18 @@ console.log(document)
         const w = 1200
         const h = 500
         const padding = 60
-    
+        d3.select('body')
+            .append('h1')
+            .attr('id', 'title')
+            .attr('style', 'color: #e86252')
+            .text('US GDP Over Time In Billions')
+
+        d3.select('body')
+            .append('div')
+            .attr('id', 'tooltip')
+            .attr('style', 'opacity: 0')
+            .attr('style', 'position: absolute')
+   
         const svg = d3.select('body')
             .append('svg')
             .attr('x', 0)
@@ -29,6 +40,9 @@ console.log(document)
         .domain([0 , d3.max(gdpValues, d => d[1])])
         .range([h - padding, padding])
         // console.log('example y scale: ',yScale(gdpValues[50][1]))
+
+
+
         svg.selectAll('rect')
             .data(gdpValues)
             .enter()
@@ -36,11 +50,22 @@ console.log(document)
             .attr('class', 'bar')
             .attr('data-date', d => d[0])
             .attr('data-gdp', d => d[1])
-            .attr('fill', 'navy')
+            .attr('fill', '#ffe74c')
             .attr('width', (w - padding * 2) / gdpValues.length)
             .attr('height', d => h- padding - yScale(d[1]))
             .attr('x', (d, i) => padding + ((w - 2 * padding) / gdpValues.length * i))
             .attr('y', d => (h - padding) - (h - padding - yScale(d[1]) ))
+            .on('mouseover', function(d) {d3.select('#tooltip').style('opacity', 1).attr('data-date', d[0]).text(`Date: ${d[0]}, GDP: ${d[1]} Billion`)})
+            .on('mouseout', function() {d3.select('#tooltip').style('opacity', 0)})
+            .on('mousemove', function() {
+                d3.select('#tooltip').style('left', (d3.event.pageX+10) + 'px').style('top', (d3.event.pageY) + 'px')
+                })
+            // .append('title')
+            // .attr('id', 'tooltip')
+            // .attr('data-date', d => d[0])
+            // .text(d => (`Date: ${d[0]}\nGDP: ${d[1]} Billion`))
+
+        
 
         const xAxis = d3.axisBottom(xScale)
         const yAxis = d3.axisLeft(yScale)
